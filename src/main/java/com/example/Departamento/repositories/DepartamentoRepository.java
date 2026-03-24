06c427bcbbd28057ad8be0592dac5dbd203ec81c
guilherme Chiquito <guilherme.fidelis2570@gmail.com>
2026-03-24 17:27:55 -0300

    adiciona repository

---
+++ src/main/java/com/example/Departamento/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.example.Departamento.repositories;
+
+import com.example.Departamento.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}
