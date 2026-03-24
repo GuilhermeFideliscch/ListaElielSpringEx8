1969a302aaf98a53ecd53114bbdb4daf340ea85f
guilherme Chiquito <guilherme.fidelis2570@gmail.com>
2026-03-24 17:25:44 -0300

    adiciona model

---
+++ src/main/java/com/example/Departamento/models/DepartamentoModel.java
@@ -0,0 +1,43 @@
+package com.example.Departamento.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_departamento")
+public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "nomeDepartamento")
+    private String nome;
+    @Column(name = "localizacaoDepartamento")
+    private String localizacao;
+
+    public DepartamentoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
